9aec7a86b80a93b64fc5898e34dbc76f0d8c41c5
Brayden Lynch <hello@braydenlynch.com>
2022-01-15 17:24:10 +1000

    Changed API scope, added profile image

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   const REDIRECT_URI = "http://localhost:3000"
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
   const RESPONSE_TYPE = "token"
+  const SCOPE = "user-read-private"
   const [token, setToken] = useState("")
 
   useEffect(() => {
@@ -21,20 +22,23 @@ function App() {
     }
   
     setToken(token)
-  
-    getUserInfo()
   }, [])
 
   const logout = () => {
     setToken("")
     window.localStorage.removeItem("token")
+    window.location.reload()
   }
 
   const [userName, setUserName] = useState("")
   const [userId, setUserId] = useState("")
   const [userImage, setUserImage] = useState("")
-  const getUserInfo = async (e) => {
-    e.preventDefault()
+  const [userCountry, setUserCountry] = useState("")
+  
+  const getUserInfo = async () => {
+    if(!token) {
+      return
+    }
     const {data} = await axios.get("https://api.spotify.com/v1/me", {
       headers: {
         Authorization: `Bearer ${token}`
@@ -44,6 +48,7 @@ function App() {
     setUserName(data.display_name)
     setUserId(data.id)
     setUserImage(data.images[0].url)
+    setUserCountry(data.country)
   }
 
   return (
@@ -51,12 +56,15 @@ function App() {
       <header className="App-header">
         <h1>Spotify Stats</h1>
         {!token 
-        ? <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
-        : <button onClick={logout}>Logout</button>
-        }
+        ? <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`}><button>Login to Spotify</button></a>
+        : <>
+        <button onClick={logout}>Logout</button>
+        <button onClick={getUserInfo}>Get User Info</button>
         <p>{userName}</p>
-        <p>{userId}</p>
-        <p>{userImage}</p>
+        <p>{userCountry}</p>
+        <img src={userImage} height="100px" width="100px" />
+        </>
+        }
       </header>
     </div>
   );
